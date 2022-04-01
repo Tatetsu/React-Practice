b7195426eaae2533eefd836d7ca115c539ef020b
Tatetsu <83476423+Tatetsu@users.noreply.github.com>
2022-04-02 04:57:16 +0900

    reactの基礎を学びました

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColofulMessage";
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColofulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
@@ -11,6 +11,17 @@ const App = () => {
   const onClickSwitchShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
   };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
